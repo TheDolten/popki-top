9a17318fb641b763c582a0178e273dea24a6a152
TheDolten <33815409+TheDolten@users.noreply.github.com>
2026-06-11 10:16:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const PAGE = body?.dataset?.page || "kus";
 let DATA = null;
 let DETAILS_OPEN = false;
 
+
 const TWITCH_CLIENT_ID = "__TWITCH_CLIENT_ID__";
 const TWITCH_SCOPES = "";
 const ACHIEVEMENTS_API_URL = "__ACHIEVEMENTS_API_URL__";
